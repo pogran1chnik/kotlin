20d8a3d18d392e31ce7a1dfd194ca667c217f09b
Dima <pogran20331@gmail.com>
2026-07-10 16:55:08 +0300

    KS-3-2: Concatenation is a change of last name

---
+++ app/src/main/kotlin/KS-3-2.kt
@@ -0,0 +1,14 @@
+package org.example.app
+
+
+fun main() {
+    val name = "Татьяна"
+    var userName = "Андреева"
+    val family = "Сергеева"
+    var age = 20
+    println(userName + " " + name + " " + family + " " + age)
+    userName = "Сидорова"
+    age = 22
+    println(userName + " " + name + " " + family + " " + age)
+}
+
