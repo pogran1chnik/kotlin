7108988a50005501005b1355727b654c87946656
Dima <pogran20331@gmail.com>
2026-07-10 17:04:09 +0300

    KS-3-2: Corrected Concatenation is a change of last name

--- app/src/main/kotlin/KS-3-2.kt
+++ app/src/main/kotlin/KS-3-2.kt
@@ -4,11 +4,11 @@ package org.example.app
 fun main() {
     val name = "Татьяна"
     var userName = "Андреева"
-    val family = "Сергеева"
+    val family = "Сергеевна"
     var age = 20
-    println(userName + " " + name + " " + family + " " + age)
+    println("["+userName + " " + name + " " + family + "," + age+ "]")
     userName = "Сидорова"
     age = 22
-    println(userName + " " + name + " " + family + " " + age)
+    println("["+userName + " " + name + " " + family + "," + age+ "]")
 }
 
