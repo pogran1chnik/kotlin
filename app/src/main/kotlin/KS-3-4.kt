38bc9e9905095be20886ff279c86b55bd8800fd0
Dima <pogran20331@gmail.com>
2026-07-11 08:18:11 +0300

    KS-3-4: String methods

---
+++ app/src/main/kotlin/KS-3-4.kt
@@ -0,0 +1,14 @@
+package org.example.app
+
+
+fun main() {
+    var whereFrom = "E2"
+    var whereTo = "E4"
+    var number = "1"
+    println("[$whereFrom-$whereTo;$number]")
+    whereFrom = "D2"
+    whereTo = "D3"
+    number = "2"
+    println("[$whereFrom-$whereTo;$number]")
+}
+
