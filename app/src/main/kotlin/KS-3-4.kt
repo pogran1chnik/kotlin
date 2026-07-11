4e3563e6d4f9f810edbc029a3ba0857d463cc31c
Dima <pogran20331@gmail.com>
2026-07-11 08:40:07 +0300

    KS-3-4: corrected

--- app/src/main/kotlin/KS-3-4.kt
+++ app/src/main/kotlin/KS-3-4.kt
@@ -5,10 +5,10 @@ fun main() {
     var whereFrom = "E2"
     var whereTo = "E4"
     var number = "1"
-    println("[$whereFrom-$whereTo;$number]")
+    println("$whereFrom-$whereTo;$number")
     whereFrom = "D2"
     whereTo = "D3"
     number = "2"
-    println("[$whereFrom-$whereTo;$number]")
+    println("$whereFrom-$whereTo;$number")
 }
 
