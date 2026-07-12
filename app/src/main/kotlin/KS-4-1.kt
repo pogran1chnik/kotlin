41032ada7e4a3ab4f5d71c4d7385da63185f4b66
Dima <pogran20331@gmail.com>
2026-07-12 08:33:51 +0300

    KS-4-1 Comparison — table reservations

---
+++ app/src/main/kotlin/KS-4-1.kt
@@ -0,0 +1,15 @@
+package org.example.app
+
+const val ALL_TABLE = 13
+
+fun main() {
+
+        val firstBookTable = 13
+        val secondBoolTable = 9
+        val availabilityTodayTable: Boolean = ALL_TABLE > firstBookTable
+        val availabilityTomorrowTable: Boolean = ALL_TABLE > secondBoolTable
+        println("Доступность столиков на сегодня: $availabilityTodayTable")
+        println("Доступность столиков на завтра: $availabilityTomorrowTable")
+    }
+
+
