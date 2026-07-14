28aceb3eea7a6b54e5be65d5bf34d966012f158c
Dima <pogran20331@gmail.com>
2026-07-14 08:59:02 +0300

    KS-4-2 Equality customs termina correctedl

--- app/src/main/kotlin/KS-4-2.kt
+++ app/src/main/kotlin/KS-4-2.kt
@@ -1,19 +1,17 @@
 package org.example.app
 
-
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_VOLUME = 100
 
 fun main() {
-        val minWeight = 35
-        val maxWeight = 100
-        val volume = 100
         val oneCargoWeight = 20
         val oneCargoVolume = 80
         val twoCargoWeight = 50
         val twoCargoVolume = 100
-        val oneAverage = minWeight < oneCargoWeight && maxWeight >= oneCargoWeight && volume > oneCargoVolume
-        val twoAverage = minWeight < twoCargoWeight && maxWeight >= twoCargoWeight && volume > twoCargoVolume
-        println("Груз с весом ${oneCargoWeight} и объемом ${oneCargoVolume} соответствует категории 'Average': ${oneAverage}")
-        println("Груз с весом ${twoCargoWeight} и объемом ${twoCargoVolume} соответствует категории 'Average': ${twoAverage}")
-    }
+
+        println("Груз с весом ${oneCargoWeight} и объемом ${oneCargoVolume} соответствует категории 'Average': ${MIN_WEIGHT < oneCargoWeight && MAX_WEIGHT >= oneCargoWeight && MAX_VOLUME > oneCargoVolume}")
+        println("Груз с весом ${twoCargoWeight} и объемом ${twoCargoVolume} соответствует категории 'Average': ${MIN_WEIGHT < twoCargoWeight && MAX_WEIGHT >= twoCargoWeight && MAX_VOLUME > twoCargoVolume}")
+}
 
 
