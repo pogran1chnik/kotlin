4c60d95ac21c23ef36dc98a9383c7895c6c98676
Dima <pogran20331@gmail.com>
2026-07-15 08:43:12 +0300

    KS-4-4 Training program corrected

--- app/src/main/kotlin/KS-4-4.kt
+++ app/src/main/kotlin/KS-4-4.kt
@@ -2,17 +2,13 @@ package org.example.app
 
 fun main() {
 
-        val day = 5
-        var arms = true
-        var legs = false
-        var back = false
-        var abs = true
+    var day = 5
 
-        arms = day % 2 == 1
-        legs = day % 2 == 0
-        back = day % 2 == 0
-        abs = day % 2 == 1
+    var arms = day % 2 == 1
+    var legs = day % 2 == 0
+    var back = day % 2 == 0
+    var abs = day % 2 == 1
 
-        println("Упражнения для рук: $arms\nУпражнения для ног: $legs\nУпражнения для спины: $back\nУпражнения для пресса: $abs")
+    println("Упражнения для рук:$arms\nУпражнения для ног:$legs\nУпражнения для спины:$back\nУпражнения для пресса:$abs")
 
 }
