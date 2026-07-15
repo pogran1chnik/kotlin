125d91f57b8e786bbee1873cdb7721e97190636a
Dima <pogran20331@gmail.com>
2026-07-15 17:28:42 +0300

    KS-4-5 departure of the ship1

--- app/src/main/kotlin/KS-4-5.kt
+++ app/src/main/kotlin/KS-4-5.kt
@@ -1,14 +1,21 @@
 package org.example.app
 
 fun main() {
+    // Ввод данных с консоли
+    println("Введите наличие повреждений корпуса (true/false):")
+    val hasDamage = readln().toBoolean()
 
-    val hasDamage = false
-    val crew = 60
-    val provision = 55
-    val weather = true
+    println("Введите текущий состав экипажа:")
+    val crew = readln().toInt()
 
+    println("Введите количество ящиков с провизией:")
+    val provision = readln().toInt()
 
-    val canSail = (!hasDamage && crew in 55..70 && provision > 50) ||
+    println("Введите благоприятность метеоусловий (true/false):")
+    val weather = readln().toBoolean()
+
+    // Единое составное условие только с логическими операторами
+    val canSail = (!hasDamage && crew >= 55 && crew <= 70 && provision > 50) ||
             (hasDamage && crew == 70 && weather && provision >= 50)
 
     println("Может ли корабль отправиться в плавание? $canSail")
