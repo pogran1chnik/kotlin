13ae4fd0a535c79f502142964bde34b2b7b8aff6
Dima <pogran20331@gmail.com>
2026-07-18 16:53:15 +0300

    KS-6-2 Timer

---
+++ app/src/main/kotlin/KS-6-2.kt
@@ -0,0 +1,13 @@
+package org.example.app
+
+fun main() {
+
+    println("Введите количество секунд:")
+    var seconds = readln().toInt()
+    while (seconds > 0) {
+        Thread.sleep(1000)
+        seconds--
+        println("Прошло $seconds секунд")
+
+    }
+}
