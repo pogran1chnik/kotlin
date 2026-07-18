f34362030800de9acf70a9474846e08451f58247
Dima <pogran20331@gmail.com>
2026-07-18 16:19:31 +0300

    KS-6-1 Autorization corrected

--- app/src/main/kotlin/KS-6-1.kt
+++ app/src/main/kotlin/KS-6-1.kt
@@ -6,13 +6,13 @@ fun main() {
     println("Создайте пароль:")
     val passwordUser = readln()
     var isAuthorized = false
-    while (isAuthorized == false) {
+    while (!isAuthorized) {
         println("Введите логин:")
         val autoLogin = readln()
         println("Введите пароль:")
         val autopassword = readln()
         if (loginUser == autoLogin && passwordUser == autopassword) {
-            println("Авторизация прошла успешно!")
+            println("Авторизация прошла успешно")
             isAuthorized = true
         } else {
             println("Неверный логин или пароль. Попробуйте снова.")
