95d437322f4016ef0cfa1e85a14707b00da89e95
Dima <pogran20331@gmail.com>
2026-07-18 16:58:26 +0300

    KS-6-2 Timer corrected

--- app/src/main/kotlin/KS-6-2.kt
+++ app/src/main/kotlin/KS-6-2.kt
@@ -1,13 +1,9 @@
 package org.example.app
 
 fun main() {
-
     println("Введите количество секунд:")
-    var seconds = readln().toInt()
-    while (seconds > 0) {
-        Thread.sleep(1000)
-        seconds--
-        println("Прошло $seconds секунд")
+    val seconds = readln().toInt()
 
-    }
+    Thread.sleep(seconds * 1000L)
+    println("Прошло $seconds секунд")
 }
