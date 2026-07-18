0595c8435d67b20e07f12786fb2d358d1f51ef15
Dima <pogran20331@gmail.com>
2026-07-18 17:13:44 +0300

    KS-6-2 Timer newlevel

--- app/src/main/kotlin/KS-6-2.kt
+++
@@ -1,9 +0,0 @@
-package org.example.app
-
-fun main() {
-    println("Введите количество секунд:")
-    val seconds = readln().toInt()
-
-    Thread.sleep(seconds * 1000L)
-    println("Прошло $seconds секунд")
-}
