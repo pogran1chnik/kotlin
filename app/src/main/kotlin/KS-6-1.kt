511a99a615ed0bd9d9ecc483259f6fe4eec6d9ec
Dima <pogran20331@gmail.com>
2026-07-18 15:55:11 +0300

    KS-6-1 Autorization

---
+++ app/src/main/kotlin/KS-6-1.kt
@@ -0,0 +1,23 @@
+package org.example.app
+
+fun main() {
+    println("Создайте логин:")
+    val loginUser = readln()
+    println("Создайте пароль:")
+    val passwordUser = readln()
+    var isAuthorized = false
+    while (isAuthorized == false) {
+        println("Введите логин:")
+        val autoLogin = readln()
+        println("Введите пароль:")
+        val autopassword = readln()
+        if (loginUser == autoLogin && passwordUser == autopassword) {
+            println("Авторизация прошла успешно!")
+            isAuthorized = true
+        } else {
+            println("Неверный логин или пароль. Попробуйте снова.")
+        }
+
+
+    }
+}
