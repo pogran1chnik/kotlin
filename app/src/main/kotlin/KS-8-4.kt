2d41e32f09178cefce9b1d637d14ef1d9af40cd3
Dima <pogran20331@gmail.com>
2026-07-21 17:18:56 +0300

    KS-8-4 ingridient

---
+++ app/src/main/kotlin/KS-8-4.kt
@@ -0,0 +1,31 @@
+package org.example.app
+
+fun main() {
+
+    val ingredients = mutableListOf("рис", "барбарис", "мясо", "морковка", "перец")
+
+
+    println("Список ингредиентов: $ingredients")
+
+
+    println("Какой ингредиент вы хотите заменить?")
+    val oldIngredient = readln().trim()
+
+
+    val index = ingredients.indexOfFirst { it.lowercase() == oldIngredient.lowercase() }
+
+    if (index != -1) {
+
+        println("На какой ингредиент вы хотите заменить?")
+        val newIngredient = readln().trim()
+
+
+        ingredients[index] = newIngredient
+
+
+        println("Готово! Вы сохранили следующий список: $ingredients")
+    } else {
+
+        println("Ингредиент '$oldIngredient' не найден в списке")
+    }
+}
