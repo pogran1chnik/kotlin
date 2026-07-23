bd115d2c54ca11284710cfc31451e3f8238061f3
Dima <pogran20331@gmail.com>
2026-07-23 15:56:42 +0300

    KS-9-3 number of servings corrected

--- app/src/main/kotlin/KS-9-3.kt
+++ app/src/main/kotlin/KS-9-3.kt
@@ -3,13 +3,15 @@ package org.example.app
 fun main() {
 
     val ingredients = listOf(2, 50, 15)
-    println("Для одной порции омлета нам понадобится: яйцо - ${ingredients[0]} шт, молоко - ${ingredients[1]} мл, сливочное масло - ${ingredients[2]} гр ")
+
+
     println("Какое количество порций?")
+
     val userNumberOfServings = readln().toInt()
-    val eggs = ingredients[0] * userNumberOfServings
-    val milk = ingredients[1] * userNumberOfServings
-    val butter = ingredients[2] * userNumberOfServings
 
-    println("На $userNumberOfServings порций вам понадобится: Яиц - $eggs, молока - $milk, сливочного масла - $butter")
 
+    val result = ingredients.map { it * userNumberOfServings }
+
+
+    println("На $userNumberOfServings порций вам понадобится: Яиц - ${result[0]}, молока - ${result[1]}, сливочного масла - ${result[2]}")
 }
