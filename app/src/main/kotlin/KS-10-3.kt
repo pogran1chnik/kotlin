d362682adae6d2c8955b254850f50ebb846ac37d
Dima <pogran20331@gmail.com>
2026-07-26 09:03:25 +0300

    KS-10-3 fun passwd

---
+++ app/src/main/kotlin/KS-10-3.kt
@@ -0,0 +1,32 @@
+package org.example.app
+
+import kotlin.random.Random
+
+
+fun main() {
+
+    print("Введите длину пароля:")
+    val length = readln().toInt()
+    val password = generatePassword(length)
+    println("Ваш пароль: $password")
+}
+
+fun generatePassword(length: Int): String {
+    val digits = "0123456789"
+    val specials = "!\"#$%&'()*+,-./ "
+    var passwd = ""
+    for (i in 0 until length) {
+        if (i % 2 == 0) {
+            val randomIndex = Random.nextInt(0, 10)
+            val digit = digits[randomIndex]
+            passwd += digit
+
+        } else {
+            val randomSpecial = Random.nextInt(0, 16)
+            val special = specials[randomSpecial]
+            passwd += special
+        }
+    }
+    return passwd
+}
+
