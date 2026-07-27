9a3fdc73f0f3043c7793145732f193b073ad3be3
Dima <pogran20331@gmail.com>
2026-07-27 08:19:44 +0300

    KS-10-4 game1

---
+++ app/src/main/kotlin/KS-10-4.kt
@@ -0,0 +1,48 @@
+package org.example.app
+
+import kotlin.random.Random
+
+fun rollDice(): Int {
+    return (1..6).random()
+}
+
+fun playRound(roundNumber: Int): Boolean {
+    println("Раунд $roundNumber:")
+    val userRoll = rollDice()
+    val computerRoll = rollDice()
+    println("Вам выпало: $userRoll")
+    println("Компьютеру выпало: $computerRoll")
+    if (userRoll > computerRoll) {
+        println("Вы выиграли раунд!")
+        return true
+    } else if (userRoll < computerRoll) {
+        println("Вы проиграли раунд!")
+        return false
+    } else {
+        println("Ничья!")
+        return false
+    }
+
+}
+
+fun main() {
+
+    var wins = 0
+    var round = 1
+    while (true) {
+        val isWin = playRound(round)
+        if (isWin) {
+            wins++
+        }
+        println("Хотите бросить игральную кость еще раз? Введите Да или Нет")
+        val answer = readln()
+        if (answer.equals("Нет", ignoreCase = true)) {
+            break
+
+        }
+        round++
+    }
+    println("Количество выигранных партий: $wins")
+}
+
+
