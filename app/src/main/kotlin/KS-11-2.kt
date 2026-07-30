ce82bd6423769ed7b218fef9858345f07beb8de6
Dima <pogran20331@gmail.com>
2026-07-30 17:37:08 +0300

    KS-11-2 Properties — User profile corrected1

---
+++ app/src/main/kotlin/KS-11-2.kt
@@ -0,0 +1,57 @@
+package org.example.app
+
+
+class  User2(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val email: String,
+    var bio: String,
+) {
+    fun printInfo() {
+        println("информация о пользователе:")
+        println("ID: ${this.id}")
+        println("login: ${this.login}")
+        println("password: ${this.password}")
+        println("email: ${this.email}")
+        println("bio: ${this.bio}")
+    }
+
+    fun readBio() {
+        println("Введите информацию о себе:")
+        val readUser = readln()
+        bio = readUser
+    }
+
+    fun changePassword() {
+        println("Введите текущий пароль:")
+        val oldPassword = readln()
+
+        if (password == oldPassword) {
+            println("Введите новый пароль:")
+            val newPassword = readln()
+            password = newPassword
+            println("Пароль изменен!")
+
+
+        } else {
+            println("Неверный пароль")
+        }
+    }
+}
+fun main() {
+    val user = User2(1, "dima", "dima123", "dima123@mail.com", "")
+    println("Начальная информация")
+    user.printInfo()
+    println()
+    println("Заполнение информации о себе")
+    user.readBio()
+    println()
+    println("Смена пароля")
+    user.changePassword()
+    println()
+    println("Обновленная информация")
+    user.printInfo()
+    println()
+}
+
