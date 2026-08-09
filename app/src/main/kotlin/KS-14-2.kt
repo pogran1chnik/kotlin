1f9b43176ca29dfb2e8e0684556c4d194d1d47aa
Dima <pogran20331@gmail.com>
2026-08-09 07:37:56 +0300

    KS-14-2 override — корабли с погрузкой

--- app/src/main/kotlin/KS-14-2.kt
+++ app/src/main/kotlin/KS-14-2.kt
@@ -1,6 +1,6 @@
 package org.example.app
 
-open class Liner(
+open class Liner1(
     val name: String,
     open var speed: Int = 30,
     open var capacity: Int = 500,
@@ -10,48 +10,56 @@ open class Liner(
         println("Скорость: $speed")
         println("Вместимость: $capacity")
     }
+    open fun load(){
+        println("выдвигает горизонтальный трап")
+    }
 }
 
-class CargoShip(
+class CargoShip1(
     name: String,
     val cargoCapacity: Int = 1000
-    ) : Liner(name) {
+    ) : Liner1(name) {
     override var speed: Int = 15
     override var capacity: Int = 200
     override fun printInfo() {
-        println("Название: $name")
-        println("Скорость: $speed")
-        println("Вместимость: $capacity")
         super.printInfo()
         println("Грузоподъемность: $cargoCapacity")
     }
 
+    override fun load() {
+        println("активирует погрузочный кран")
+    }
+
 }
 
-class Icebreaker(name: String) : Liner(name) {
+class Icebreaker1(name: String) : Liner1(name) {
     override var speed: Int = 10
     override var capacity: Int = 100
     val canBreakIce: Boolean = true
     override fun printInfo() {
-        println("Название: $name")
-        println("Скорость: $speed")
-        println("Вместимость: $capacity")
         super.printInfo()
         println("Ломает леда: ${if (canBreakIce) "да" else "нет"}")
     }
 
+    override fun load() {
+        println("открывает ворота со стороны кормы")
+    }
 }
 fun main() {
 
     println("Лайнер")
-    val liner1 = Liner(name = "Titanik")
+    val liner1 = Liner1(name = "Titanik")
     liner1.printInfo()
+    liner1.load()
 
     println("Грузовой корабль")
-    val cargo = CargoShip("BMRT", 100)
+    val cargo = CargoShip1("BMRT", 100)
     cargo.printInfo()
+    cargo.load()
+
 
     println("Ледокол")
-    val icebreaker = Icebreaker("Arctika")
+    val icebreaker = Icebreaker1("Arctika")
     icebreaker.printInfo()
+    icebreaker.load()
 }
