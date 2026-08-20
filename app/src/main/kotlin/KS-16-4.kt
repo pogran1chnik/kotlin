aa2504ed5457f054f827fa3de1942906dbef126c
Dima <pogran20331@gmail.com>
2026-08-20 16:45:13 +0300

    KS-16-4 encapsulation logic заказ в интернет-магазине corrected

--- app/src/main/kotlin/KS-16-4.kt
+++ app/src/main/kotlin/KS-16-4.kt
@@ -2,39 +2,42 @@ package org.example.app
 
 class Order(
     private val orderId: Int,
-    private var status: String,
-){
-    fun getStatus(): String{
+    private var status: String
+) {
+    fun getStatus(): String {
         return status
     }
-    fun getOrder(): Int{
+
+    fun getOrder(): Int {
         return orderId
     }
+
     private fun updateStatus(newStatus: String) {
         status = newStatus
     }
-        fun prinInfo(){
+
+    fun changeStatus(newStatus: String) {
+        println("Отправка заявки менеджеру...")
+        this.updateStatus(newStatus)
+        println("Статус изменен на: $newStatus")
+    }
+
+    fun printInfo() {
         println("Заказ #$orderId: статус '$status'")
     }
-
-    fun manageOrder(order: Order, newStatus: String) {
-        println("Отправка заявки менеджеру...")
-        order.updateStatus(newStatus)
-        println("Статус изименен на: $newStatus")
-    }
 }
-fun main() {
 
+fun main() {
     val order = Order(1, "Новый")
+
     println("Информация о заказе")
-    order.prinInfo()
+    order.printInfo()
     println()
 
     println("Изменение статуса")
-    order.manageOrder(order, "В обработке")
+    order.changeStatus("В обработке")
     println()
 
     println("Обновленная информация")
-    order.prinInfo()
-
+    order.printInfo()
 }
