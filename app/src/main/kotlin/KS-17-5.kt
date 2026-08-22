6bfd39bf3655896397746c177e80f3ed7204aa04
Dima <pogran20331@gmail.com>
2026-08-22 08:19:13 +0300

    KS-17-5 backing property логин и пароль

---
+++ app/src/main/kotlin/KS-17-5.kt
@@ -0,0 +1,42 @@
+package org.example.app
+
+class User(login: String, password: String){
+    private var _login: String = login
+    private var _password: String = password
+
+    var login: String
+        get() = _login
+        set(value) {
+            _login = login
+            println("Логин изменен!")
+        }
+    var password: String
+        get() = "*".repeat(_password.length)
+        set(value) {
+            println("Вы не можете изменить пароль")
+        }
+    fun printInfo(){
+        println("Логин: $_login")
+        println("Пароль: $_password")
+        println()
+    }
+}
+
+fun main() {
+
+
+    val user = User("admin", "12345")
+
+    println("=== Создание пользователя ===")
+    user.printInfo()
+
+    println("=== Попытка изменить пароль ===")
+    user.password = "newPassword"
+    println("После попытки изменения:")
+    user.printInfo()
+
+    println("=== Успешная смена логина ===")
+    user.login = "super_admin"
+    user.printInfo()
+
+}
