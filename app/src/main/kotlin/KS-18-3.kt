11739d7bb8e40c773ec6c66876c8696a9d09a57d
Dima <pogran20331@gmail.com>
2026-08-23 08:22:56 +0300

    KS-18-3 smart cast тамагочи с питомцами corrected

--- app/src/main/kotlin/KS-18-3.kt
+++ app/src/main/kotlin/KS-18-3.kt
@@ -2,25 +2,28 @@ package org.example.app
 
 abstract class Animal(
     val name: String
-){
+) {
     abstract fun eat()
-}
 
+    fun sleep() {
+        println("$name -> спит")
+    }
+}
 class Fox(name: String): Animal(name){
     override fun eat() {
-        println("$name -> ягоды")
+        println("$name -> ест ягоды")
 
     }
 }
 class Dog(name: String): Animal(name) {
     override fun eat() {
-        println("$name -> кости")
+        println("$name -> грызет кости")
 
     }
 }
 class Cat(name: String): Animal(name) {
     override fun eat() {
-        println("$name -> рыба")
+        println("$name -> ест рыбу")
 
     }
 }
@@ -31,5 +34,5 @@ fun main() {
     val animals: List<Animal> = listOf(Fox("Рыжик"), Dog("Бобик"), Cat("Мурка"))
     animals.forEach { it.eat() }
 
-
+    animals.forEach { it.sleep() }
 }
